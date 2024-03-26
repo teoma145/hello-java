4add1f7deb0d414e67d1ea9340da570fe289ba5e
teoma145 <matteodark145@yahoo.it>
2024-03-26 12:57:22 +0100

    first commit HelloJava

---
+++ src/org/learning/basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.learning.basics;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println(5+3);
+
+    }
+}
